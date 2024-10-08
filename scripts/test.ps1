a47d7a610abdf1cd14248f8d5781a58a39118008
Glatzel <893016099@qq.com.com>
2024-10-08 20:36:57 +0800

    fix

---
+++ scripts/test.ps1
@@ -0,0 +1,3 @@
+Set-Location $PSScriptRoot
+Set-Location ..
+pixi run pytest "./src" --benchmark-max-time=0.00005 --benchmark-min-rounds=1 --benchmark-histogram=histogram/audio --cov --cov-report=html
